b665f420e8500847543a8bacbd95192ce214bea2
MelissaGarciaGitHub <106970355+MelissaGarciaGitHub@users.noreply.github.com>
2023-11-09 14:37:11 -0500

    Update server.js

--- ProjectPortScanner/server.js
+++ ProjectPortScanner/server.js
@@ -3,76 +3,81 @@ const net = require('net');
 const cors = require('cors');
 
 const app = express();
-
 app.use(express.json());
 
-// Set up CORS to allow your client domain
 const corsOptions = {
-  origin: 'https://mgarcia46.domains.ggc.edu', // Replace with your actual client domain
-  methods: ['POST'], // Allow only POST for /scan endpoint
+  origin: 'https://mgarcia46.domains.ggc.edu',
+  methods: ['POST'],
   optionsSuccessStatus: 200
 };
 
-app.use(cors(corsOptions)); // Enable CORS with the specified options
+app.use(cors(corsOptions));
 
-// Define the /scan POST route
-// ... [rest of your code]
+// New asynchronous function to scan ports with delay
+async function scanPorts(ip, ports) {
+  const scanResults = [];
 
-app.post('/scan', (req, res) => {
-    const { ip } = req.body;
-    console.log('Received IP for scanning:', ip);
+  for (const port of ports) {
+    const result = await scanPort(ip, port);
+    scanResults.push(result);
+    await delay(100); // Wait for 100 ms between each scan
+  }
+
+  return scanResults;
+}
+
+function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function scanPort(ip, port) {
+  return new Promise(resolve => {
+    const socket = new net.Socket();
+    const result = { port: port, status: 'closed' };
+
+    socket.setTimeout(10000); // Increased timeout
+    socket.on('connect', () => {
+      result.status = 'open';
+      socket.end();
+    });
+    socket.on('error', () => {
+      socket.destroy();
+    });
+    socket.on('timeout', () => {
+      socket.destroy();
+    });
+    socket.on('close', () => {
+      resolve(result);
+    });
+
+    socket.connect({ port: port, host: ip });
+  });
+}
+
+// New /scan route using the asynchronous scan
+app.post('/scan', async (req, res) => {
+  const { ip } = req.body;
+  const funport = [20, 21, 22, 23, 25, 53, 80, 137, 139, 443, 445, 1433, 1434, 3306, 3389, 8080, 8443];
   
-    const funport = [20, 21, 22, 23, 25, 53, 80, 137, 139, 443, 445, 1433, 1434, 3306, 3389, 8080, 8443];
-    let portScans = funport.map(port => {
-        return new Promise((resolve, reject) => {
-            const socket = new net.Socket();
-            const result = { port: port, status: 'closed' };
-
-            socket.setTimeout(5000); // Increased timeout
-            socket.on('connect', () => {
-                result.status = 'open';
-                socket.destroy();
-                resolve(result);
-            });
-            socket.on('error', (err) => {
-                console.log(`Error on port ${port}: ${err.message}`); // Log error message
-                socket.destroy();
-                resolve(result); // Still resolve the result, as we know the port is closed
-            });
-            socket.on('timeout', () => {
-                console.log(`Timeout on port ${port}`); // Log timeout
-                socket.destroy();
-                resolve(result);
-            });
-            socket.on('close', () => {
-                resolve(result);
-            });
-
-            socket.connect({ port: port, host: ip });
-        });
-    });
-
-    Promise.all(portScans).then(results => {
-        console.log('Scan results:', results); // Log the results
-        res.json(results);
-    }).catch(error => {
-        console.error('Error during scanning:', error);
-        res.status(500).send('Error during scanning');
-    });
+  try {
+    const results = await scanPorts(ip, funport);
+    res.json(results);
+  } catch (error) {
+    res.status(500).send('Error during scanning');
+  }
 });
 
-// ... [rest of your code]
- 
-app.use((req, res) =>{
-  res.send('Welcome to Port Scan Server By Melissa and Jessica!')
-})
+// Welcome route
+app.get('/', (req, res) => {
+  res.send('Welcome to Port Scan Server!');
+});
+
+// 404 handler
 app.use((req, res) => {
   res.status(404).send('404 Not Found: The requested resource does not exist on this server.');
-})
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-
