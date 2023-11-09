f95edb4b76123d7ce56d9edeef0e6803b6c63eb6
MelissaGarciaGitHub <106970355+MelissaGarciaGitHub@users.noreply.github.com>
2023-11-09 14:16:41 -0500

    Update server.js

--- ProjectPortScanner/server.js
+++ ProjectPortScanner/server.js
@@ -16,46 +16,55 @@ const corsOptions = {
 app.use(cors(corsOptions)); // Enable CORS with the specified options
 
 // Define the /scan POST route
+// ... [rest of your code]
+
 app.post('/scan', (req, res) => {
-    const { ip } = req.body; // Extract the IP address from the request body
-   console.log('Received IP:', ip);
+    const { ip } = req.body;
+    console.log('Received IP for scanning:', ip);
   
- const funport = [20, 21, 22, 23, 25, 53, 80, 137, 139, 443, 445, 1433, 1434, 3306, 3389, 8080, 8443];
+    const funport = [20, 21, 22, 23, 25, 53, 80, 137, 139, 443, 445, 1433, 1434, 3306, 3389, 8080, 8443];
     let portScans = funport.map(port => {
-        return new Promise(resolve => {
+        return new Promise((resolve, reject) => {
             const socket = new net.Socket();
             const result = { port: port, status: 'closed' };
 
-            socket.setTimeout(4000);
+            socket.setTimeout(5000); // Increased timeout
             socket.on('connect', () => {
                 result.status = 'open';
                 socket.destroy();
+                resolve(result);
             });
-            socket.on('error', () => {
+            socket.on('error', (err) => {
+                console.log(`Error on port ${port}: ${err.message}`); // Log error message
                 socket.destroy();
+                resolve(result); // Still resolve the result, as we know the port is closed
             });
             socket.on('timeout', () => {
+                console.log(`Timeout on port ${port}`); // Log timeout
                 socket.destroy();
+                resolve(result);
             });
             socket.on('close', () => {
                 resolve(result);
             });
 
-            socket.connect({ port: port, host: ip }); // Ensure to connect using host and port
+            socket.connect({ port: port, host: ip });
         });
     });
 
     Promise.all(portScans).then(results => {
-        console.log(results)
-        res.json(results); // Respond with the results as JSON
+        console.log('Scan results:', results); // Log the results
+        res.json(results);
     }).catch(error => {
+        console.error('Error during scanning:', error);
         res.status(500).send('Error during scanning');
     });
 });
 
-   
+// ... [rest of your code]
+ 
 app.use((req, res) =>{
-  res.send('Welcome to Port Scan Server!')
+  res.send('Welcome to Port Scan Server By Melissa and Jessica!')
 })
 app.use((req, res) => {
   res.status(404).send('404 Not Found: The requested resource does not exist on this server.');
