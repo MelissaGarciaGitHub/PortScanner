e94c61e48332d047afeb346ef8697c69ae8a0b64
MelissaGarciaGitHub <106970355+MelissaGarciaGitHub@users.noreply.github.com>
2023-11-08 23:22:55 -0500

    Update server.js

--- ProjectPortScanner/server.js
+++ ProjectPortScanner/server.js
@@ -3,27 +3,21 @@ const net = require('net');
 const cors = require('cors');
 
 const app = express();
-
 app.use(express.json());
-// Other code remains the same
 
+// Set up CORS to allow your client domain
 const corsOptions = {
-  origin: 'https://mgarcia46.domains.ggc.edu', // Set this to your front-end's origin
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true, // if your front-end would send cookies to the server
-  optionsSuccessStatus: 204
+  origin: 'https://mgarcia46.domains.ggc.edu', // Replace with your actual client domain
+  methods: ['POST'], // Allow only POST for /scan endpoint
+  optionsSuccessStatus: 200
 };
 
-app.use(cors(corsOptions)); // Enable CORS with the above options
+app.use(cors(corsOptions)); // Enable CORS with the specified options
 
-// The rest of the code remains the same
-
-
+// Define the /scan POST route
 app.post('/scan', (req, res) => {
-    const { ip } = req.body; 
-    const funport = [20, 21, 22, 23, 25, 53, 80, 137, 139, 443, 445, 1433, 1434, 3306, 3389, 8080, 8443]; 
-    let scanResults = [];
-
+    const { ip } = req.body; // Make sure this key matches what the client sends
+    const funport = [20, 21, 22, 23, 25, 53, 80, 137, 139, 443, 445, 1433, 1434, 3306, 3389, 8080, 8443];
     let portScans = funport.map(port => {
         return new Promise(resolve => {
             const socket = new net.Socket();
@@ -44,14 +38,17 @@ app.post('/scan', (req, res) => {
                 resolve(result);
             });
 
-            socket.connect(port, ip);
+            socket.connect({ port: port, host: ip }); // Ensure to connect using host and port
         });
     });
 
     Promise.all(portScans).then(results => {
-        res.json(results);
+        res.json(results); // Respond with the results as JSON
+    }).catch(error => {
+        res.status(500).send('Error during scanning');
     });
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
