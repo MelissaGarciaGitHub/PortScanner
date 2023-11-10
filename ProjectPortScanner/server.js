efd1d6f5e2772ba4add2ccd3a0cb3779151f026d
MelissaGarciaGitHub <106970355+MelissaGarciaGitHub@users.noreply.github.com>
2023-11-10 10:21:13 -0500

    Update server.js

--- ProjectPortScanner/server.js
+++ ProjectPortScanner/server.js
@@ -61,3 +61,6 @@ app.use((req, res) =>{
 app.use((req, res) => {
   res.status(404).send('404 Not Found: The requested resource does not exist on this server.');
 })
+
+const PORT = process.env.PORT || 3000;
+
